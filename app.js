1356ebf585f84588d1184c4520af5d50a250a316
Adipon Das <51257162+adipondas@users.noreply.github.com>
2024-04-24 11:40:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    // In order to use PUT HTTP verb to edit item
+    methodOverride = require('method-override'),
+    // Mitigate XSS using sanitizer
+    sanitizer = require('sanitizer'),
+    app = express(),
+    port = 8000
 
-const app = express();
-const port = 3000;
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+// https: //github.com/expressjs/method-override#custom-logic
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method;
+        delete req.body._method;
+        return method
+    }
+}));
 
 // Middleware
 app.set('view engine', 'ejs');
